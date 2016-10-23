5017202c2b173c3826929be6b7a73305defcbfa6
Ian Plunkett <ian.plunkett@gmail.com>
2016-10-23 13:12:37 -0700

    Going through the reactjs tutorial

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,109 @@
-alert('hello world!');
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+  render() {
+    const status = 'Next player: X';
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('container')
+);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+	return squares[a];
+    }
+  }
+    return null;
+}
+
+window.addEventListener('mousedown', function(e) {
+    document.body.classList.add('mouse-navigation');
+    document.body.classList.remove('kbd-navigation');
+});
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode === 9) {
+	document.body.classList.add('kbd-navigation');
+	document.body.classList.remove('mouse-navigation');
+    }
+});
+window.addEventListener('click', function(e) {
+    if (e.target.tagName === 'A' && e.target.getAttribute('href') === '#') {
+	e.preventDefault();
+    }
+});
+window.onerror = function(message, source, line, col, error) {
+    var text = error ? error.stack || error : message + ' (at ' + source + ':' + line + ':' + col + ')';
+    errors.textContent += text + '\n';
+    errors.style.display = '';
+};
+console.error = (function(old) {
+    return function error() {
+	errors.textContent += Array.prototype.slice.call(arguments).join(' ') + '\n';
+	errors.style.display = '';
+	old.apply(this, arguments);
+    }
+})(console.error);
