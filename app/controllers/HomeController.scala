e0ba40cc2b6fdd1a20abcd6dfefccff0603d6fe8
Ian Plunkett <ian.plunkett@gmail.com>
2016-10-23 09:01:56 -0700

    Playing with session handling

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,6 +5,15 @@ import play.api.mvc._
 class HomeController extends Controller {
 
   def index = Action {
-    Ok("It works!")
+    Ok("It works!").withSession(
+    "connected" -> "ian.plunkett@gmail.com")
+  }
+
+  def session = Action { request =>
+    request.session.get("connected").map { user =>
+      Ok("Hello " + user)
+    }.getOrElse {
+      Unauthorized("Oops, you are not connected")
+    }
   }
 }
